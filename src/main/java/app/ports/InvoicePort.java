7d75685143b1f9b5e593fe8c01876a39b5957f4e
SebastianMontoya <montoyasebastian23@gmail.com>
2025-03-10 19:50:03 -0500

    se agregab cambios

--- src/main/java/app/ports/InvoicePort.java
+++ src/main/java/app/ports/InvoicePort.java
@@ -4,10 +4,19 @@
  */
 package app.ports;
 
+import app.domain.models.Invoice;
+import java.util.List;
+import app.domain.models.Person;
+import app.domain.models.User;
+import app.domain.models.Pet;
 /**
  *
  * @author Viviana
  */
 public interface InvoicePort {
+
+    public List<Invoice> getAllInvoices();
+
+    
     
 }
--- src/main/java/app/ports/UserPort.java
+++ src/main/java/app/ports/UserPort.java
@@ -3,11 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package app.ports;
+import app.domain.models.User;
+import app.domain.models.Person;
+
 
 /**
  *
  * @author Viviana
  */
 public interface UserPort {
+      
+    public void saveUser(User user);
+    public boolean existUserName(String username);
+    public User findByUsername(Person person);
+
+    
+
     
 }
